e6ae84db7819ef46eeb1be4203a060f3ec1f58b1
Anurag Gupta <anuraggupta29@outlook.com>
2020-08-27 17:13:22 +0530

    updated files

--- linkedin_send_all.py
+++ linkedin_send_all.py
@@ -18,9 +18,9 @@ def login():
 
 	except:
 		username = driver.find_element_by_id("session_key") # Getting the login element
-		username.send_keys("anuraggupta29@outlook.com") # Sending the keys for username
+		username.send_keys("enter your email here") # Sending the keys for username
 		password = driver.find_element_by_id("session_password") # Getting the password element
-		password.send_keys("Agrox29@arun") # Sending the keys for password
+		password.send_keys("enter your password here") # Sending the keys for password
 		driver.find_element_by_class_name("sign-in-form__submit-button").click() # Getting the tag for submit button
 
 def goto_network():
